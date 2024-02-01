eac7b58e8f38403c89baec8ff47095745ded1ffb
Tatyana <99961462+TatyanaDaviduk@users.noreply.github.com>
2024-02-01 11:35:04 +0300

    slider

---
+++ slider/script/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.my-slider',{
+    slidesPerView: 2,
+    spaceBetween: 15,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
